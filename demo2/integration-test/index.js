465888f9241bf2bb91c73942fee34c7bad467cff
Dave Kerr <dwmkerr@gmail.com>
2016-07-14 12:46:21 +0800

    Added demo2

---
+++ demo2/integration-test/index.js
@@ -0,0 +1,34 @@
+//  index.js
+//
+//  Entrypoint for the 'node-docker-microservice' integration tests.
+//  Note that due to:
+//
+//  https://github.com/visionmedia/supertest/issues/314
+//  
+//  If the service isn't running, you'll get failures like:
+//
+//  Cannot read property 'status' of undefined
+//
+//  For test failures.
+var supertest = require('supertest');
+var should = require('should');
+
+describe('users-service', () => {
+
+  var api = supertest('http://localhost:8123');
+
+  it('returns a 200 for a known user', (done) => {
+
+    api.get('/search?email=homer@thesimpsons.com')
+      .expect(200, done);
+
+  });
+  
+  it('returns a 404 for an unknown user', (done) => {
+
+    api.get('/search?email=bender@futurama.com')
+      .expect(404, done);
+
+  })
+
+});
